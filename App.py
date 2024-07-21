603ece085f81ecbb0ffea35952490b25fb33f4cf
Nilava Biswas <nilavabiswas93@gmail.com>
2024-07-21 19:38:54 +0530

    alignment fixed

--- App.py
+++ App.py
@@ -12,9 +12,12 @@ def generatemessage(text: str):
         messages=[
             {
                 'role': 'user',
-                'content': """label all the primary key emotions and its sentiments as postive or negative sentiment with 
+                'content': """label all the primary key emotions and 
+                its sentiments as postive or negative sentiment with 
                 corresponding scores in 
-                order of emotion,sentiment score as 'sentiment_val' in negative or postive integer and emotion score as 'emo_val'  from
+                order of emotion,sentiment score as 'sentiment_val' 
+                in negative or postive integer and emotion score as 'emo_val' 
+             from
                 the text after ''' and create a json"+"'''"""+text
             },
         ],
